8812fda07a8328b971c87e2ad57bca98e62be995
WOWS <023zhangjiaoshou@gmail.com>
2019-06-09 22:47:08 -0700

    Update Img_Utility.h

--- lib/Img_Utility.h
+++ lib/Img_Utility.h
@@ -3,11 +3,6 @@
 #include <cmath>
 #include <iostream>
 #include <fstream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/ml/ml.hpp>  
 #include <memory>
 #include <queue>
 #include <unordered_map>
